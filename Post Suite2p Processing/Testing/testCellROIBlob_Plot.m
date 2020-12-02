12c2978adaffe4de7a0fc8120ebfa6f8c76841ed
CommanderPho <PhoHale@gmail.com>
2020-12-02 17:10:45 -0500

    working selection

--- Post Suite2p Processing/Testing/testCellROIBlob_Plot.m
+++ Post Suite2p Processing/Testing/testCellROIBlob_Plot.m
@@ -6,6 +6,8 @@ black3DArray = fnBuildCDataFromConstantColor([0.0 0.0 0.0], desiredSize);
 darkgrey3DArray = fnBuildCDataFromConstantColor([0.3 0.3 0.3], desiredSize);
 lightgrey3DArray = fnBuildCDataFromConstantColor([0.6 0.6 0.6], desiredSize);
 
+orange3DArray = fnBuildCDataFromConstantColor([0.9 0.3 0.1], desiredSize);
+
 red3DArray = fnBuildCDataFromConstantColor([1.0 0.0 0.0], desiredSize);
 green3DArray = fnBuildCDataFromConstantColor([0.0 1.0 0.0], desiredSize);
 blue3DArray = fnBuildCDataFromConstantColor([0.0 0.0 1.0], desiredSize);
@@ -35,7 +37,7 @@ for i = 1:final_data_explorer_obj.num_cellROIs
         
         if isnan(currEdgePlotImageIdentifier.edgeOffsetIndex)
             % For the fill layer, the edgeOffsetIndex is nan
-            imagePlotHandles(i, plotImageIndex) = image('CData', colorsArray{plotImageIndex}, 'AlphaData', final_data_explorer_obj.getFillRoiMask(i));
+            imagePlotHandles(i, plotImageIndex) = image('CData', colorsArray{plotImageIndex}, 'AlphaData', (0.5 * final_data_explorer_obj.getFillRoiMask(i)));
 %             imagePlotHandles(i, plotImageIndex).ButtonDownFcn = {@testCellROIBlob_Plot_OnClicked_Callback, final_data_explorer_obj};
         else
             imagePlotHandles(i, plotImageIndex) = image('CData', colorsArray{plotImageIndex}, 'AlphaData', final_data_explorer_obj.getEdgeOffsetRoiMasks(currEdgePlotImageIdentifier.edgeOffsetIndex, i));
@@ -56,15 +58,22 @@ title('Combined Insets and Outsets')
 set(gca,'xtick',[],'YTick',[])
 set(gca,'xlim',[1 512],'ylim',[1 512])
 
+%% Build Interaction Helper Object:
+interaction_helper_obj = InteractionHelper(final_data_explorer_obj, 'Pho');
+interaction_helper_obj.imagePlotHandles = imagePlotHandles;
+interaction_helper_obj.activeFigure = testCellROIBlob_Plot_figH;
+
+
 dcm = datacursormode(testCellROIBlob_Plot_figH);
 dcm.Enable = 'on';
 dcm.DisplayStyle = 'window';
 if exist('slider_controller','var')
-    dcm.UpdateFcn = @(figH, info) (testCellROIBlob_Plot_Callback(figH, info, final_data_explorer_obj, slider_controller));
+    dcm.UpdateFcn = @(figH, info) (testCellROIBlob_Plot_Callback(figH, info, interaction_helper_obj, slider_controller));
 else
-    dcm.UpdateFcn = @(figH, info) (testCellROIBlob_Plot_Callback(figH, info, final_data_explorer_obj));
+    dcm.UpdateFcn = @(figH, info) (testCellROIBlob_Plot_Callback(figH, info, interaction_helper_obj));
 end
         
+
 
 %% Update the Plots after creation:
 
@@ -80,27 +89,63 @@ end
 
 
 %% Custom ToolTip callback function that displays the clicked cell ROI as well as the x,y position.
-function txt = testCellROIBlob_Plot_Callback(figH, info, final_data_explorer_obj, activeSliderController)
+function txt = testCellROIBlob_Plot_Callback(figH, info, interaction_helper_obj, activeSliderController)
+    persistent desiredSize
+    persistent orange3DArray
+    persistent lightgrey3DArray
+    desiredSize = [512 512];
+    orange3DArray = fnBuildCDataFromConstantColor([0.9 0.3 0.1], desiredSize);
+    lightgrey3DArray = fnBuildCDataFromConstantColor([0.6 0.6 0.6], desiredSize);
+    % interaction_helper_obj.final_data_explorer_obj
     x = info.Position(1);
     y = info.Position(2);
-    uniqueCompIndex = final_data_explorer_obj.amalgamationMasks.cellROI_LookupMask(y, x); % Figure out explicitly what index type is assigned here.
+    uniqueCompIndex = interaction_helper_obj.final_data_explorer_obj.amalgamationMasks.cellROI_LookupMask(y, x); % Figure out explicitly what index type is assigned here.
     cellROIString = '';
     if uniqueCompIndex > 0
         fprintf('selected cellROI: %d...\n', uniqueCompIndex);
-        cellROI_CompName = final_data_explorer_obj.uniqueComps{uniqueCompIndex};
+        cellROI_CompName = interaction_helper_obj.final_data_explorer_obj.uniqueComps{uniqueCompIndex};
         cellROIString = ['[' num2str(uniqueCompIndex) ']' cellROI_CompName];
+        
+        % Ask interaction_helper_obj to toggle the selection status.
+        
+        [interaction_helper_obj, curr_cellROI_IsSelected] = interaction_helper_obj.toggleCellRoiIsSelected(uniqueCompIndex);
+        
+        %% Update Selections graphically:
+        
+        % Get the fill handle
+        curr_sel_fill_im_h = interaction_helper_obj.imagePlotHandles(uniqueCompIndex, 1);
+%         updated_alpha_data = interaction_helper_obj.final_data_explorer_obj.getFillRoiMask(uniqueCompIndex);
+        if curr_cellROI_IsSelected
+%             updated_alpha_data = updated_alpha_data .* 0.9;
+            
+            updated_color_data = orange3DArray;
+        else
+%             updated_alpha_data = updated_alpha_data .* 0.5;
+            
+            updated_color_data = lightgrey3DArray;
+        end
+%         set(curr_sel_fill_im_h,'CData', updated_color_data, 'AlphaData', updated_alpha_data);
+        
+        set(curr_sel_fill_im_h,'CData', updated_color_data);
+        drawnow
+%         for i = 1:size(interaction_helper_obj.imagePlotHandles, 1)
+%             for j = size(interaction_helper_obj.imagePlotHandles, 2)
+%         %         imagePlotHandles(i, j).ButtonDownFcn = {@testCellROIBlob_Plot_OnClicked_Callback, final_data_explorer_obj};
+%                 interaction_helper_obj.imagePlotHandles(uniqueCompIndex, j).ButtonDownFcn = @(hObject, eventData) (fnTestCellROIBlob_Plot_OnClicked_Callback(hObject, eventData, final_data_explorer_obj));
+%             end
+%         end        
         
 %         cellROIString = num2str(uniqueCompIndex);  
         
         
-        cellROI_PreferredLinearStimulusIndicies = squeeze(final_data_explorer_obj.preferredStimulusInfo.PreferredStimulus_LinearStimulusIndex(uniqueCompIndex,:)); % These are the linear stimulus indicies for this all sessions of this datapoint.
+        cellROI_PreferredLinearStimulusIndicies = squeeze(interaction_helper_obj.final_data_explorer_obj.preferredStimulusInfo.PreferredStimulus_LinearStimulusIndex(uniqueCompIndex,:)); % These are the linear stimulus indicies for this all sessions of this datapoint.
 %         disp(cellROI_PreferredLinearStimulusIndicies);
 
-        cellROI_PreferredAmpsFreqsIndicies = final_data_explorer_obj.stimuli_mapper.indexMap_StimulusLinear2AmpsFreqsArray(cellROI_PreferredLinearStimulusIndicies',:);
+        cellROI_PreferredAmpsFreqsIndicies = interaction_helper_obj.final_data_explorer_obj.stimuli_mapper.indexMap_StimulusLinear2AmpsFreqsArray(cellROI_PreferredLinearStimulusIndicies',:);
 %         disp(cellROI_PreferredAmpsFreqsIndicies);
 
-        cellROI_PreferredAmps = final_data_explorer_obj.uniqueAmps(cellROI_PreferredAmpsFreqsIndicies(:,1));
-        cellROI_PreferredFreqs = final_data_explorer_obj.uniqueFreqs(cellROI_PreferredAmpsFreqsIndicies(:,2));
+        cellROI_PreferredAmps = interaction_helper_obj.final_data_explorer_obj.uniqueAmps(cellROI_PreferredAmpsFreqsIndicies(:,1));
+        cellROI_PreferredFreqs = interaction_helper_obj.final_data_explorer_obj.uniqueFreqs(cellROI_PreferredAmpsFreqsIndicies(:,2));
 
 %         disp(num2str(cellROI_PreferredAmps'));
         
--- helpers/Classes/InteractionHelper.m
+++ helpers/Classes/InteractionHelper.m
@@ -1,23 +1,137 @@
-classdef SelectionHelper
-    %SELECTIONHELPER Summary of this class goes here
+classdef InteractionHelper < handle & matlab.mixin.CustomDisplay
+    %InteractionHelper Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        Property1
+        final_data_explorer_obj % FinalDataExplorer
+
+        %% Selection
+        isCellRoiSelected
+        
+        %% Annotation/Backing Store
+        AnnotatingUser
+        BackingFile
+        
+        %% Graphical (TODO: potentially refactor)
+        activeFigure
+        imagePlotHandles
+        
+    end
+    
+    methods %% Setters Method Block
+       function obj = set.final_data_explorer_obj(obj, value)
+           obj.final_data_explorer_obj = value;
+           % When the final_data_explorer_obj is set, rebuild the variables.
+           obj.isCellRoiSelected = zeros([obj.final_data_explorer_obj.num_cellROIs 1], 'logical');
+           
+       end  
+
+    end % end setters method block
+
+    %% Computed Properties:
+    properties (Dependent)
+        num_cellROIs
+        num_selectedCellROIs
     end
     
     methods
-        function obj = SelectionHelper(inputArg1,inputArg2)
-            %SELECTIONHELPER Construct an instance of this class
+       function num_cellROIs = get.num_cellROIs(obj)
+          num_cellROIs = obj.final_data_explorer_obj.num_cellROIs;
+       end
+       function num_selectedCellROIs = get.num_selectedCellROIs(obj)
+          num_selectedCellROIs = sum(obj.isCellRoiSelected, 'all');
+       end
+    end
+    
+    
+    methods
+        function obj = InteractionHelper(final_data_explorer_obj, annotatingUser, backingFilePath)
+            %InteractionHelper Construct an instance of this class
             %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+            obj.final_data_explorer_obj = final_data_explorer_obj;
+            
+            if ~exist('annotatingUser','var')
+                annotatingUser = 'Anonymous';
+            end  
+            obj.AnnotatingUser = annotatingUser;
+            if (~exist('backingFilePath','var') || isempty(backingFilePath))
+				[filename, path, ~] = uiputfile('*.mat','User CellROI Annotations Backing File',['UAnnotations-CellROI-', '0', '.mat']);
+				if isequal(filename,0) || isequal(path,0)
+				   error('User clicked Cancel.')
+                end
+				backingFilePath = fullfile(path,filename);
+            end
+            % Setup backing file
+			obj.BackingFile.fullPath = backingFilePath;
+			obj.BackingFile.hasBackingFile = false;
+            obj.BackingFile.shouldAutosaveToBackingFile = true;
+            obj.tryOpenBackingFile();
+            
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        %% Selection Methods:
+        function [obj, newIsSelected] = toggleCellRoiIsSelected(obj, uniqueCompListIndex)
+            %toggleCellRoiIsSelected Summary of this method goes here
+            %   uniqueCompListIndex: cellROI to update selection status of
+            newIsSelected = ~obj.isCellRoiSelected(uniqueCompListIndex);
+            obj.updateCellRoiIsSelected(uniqueCompListIndex, newIsSelected);
         end
-    end
+        
+        function obj = updateCellRoiIsSelected(obj, uniqueCompListIndex, newIsSelected)
+            %updateCellRoiIsSelected Summary of this method goes here
+            %   uniqueCompListIndex: cellROI to update selection status of
+            %   newIsSelected: new selection status
+            obj.isCellRoiSelected(uniqueCompListIndex) = newIsSelected;
+        end
+    end %% end methods
+    
+    %% Backing File Methods Block:
+    methods
+      
+		function tryOpenBackingFile(obj)
+			% see if the file exists at the provided path
+			if ~exist(obj.BackingFile.fullPath,'file')
+                % if it doesn't exist, create it
+				disp(['Creating new backing file at ' obj.BackingFile.fullPath])
+			else
+				disp(['Opening existing backing file at ' obj.BackingFile.fullPath])
+				% TODO: load from backing file:
+				obj = UserAnnotationsManager.loadFromExistingBackingFile(obj.BackingFile.fullPath); % will this work?
+				error('Not yet finished!')
+			end
+			
+			obj.BackingFile.matFile = matfile(obj.BackingFile.fullPath,'Writable',true);
+			obj.createBackingFile();
+			obj.BackingFile.hasBackingFile = true;
+
+		end
+		
+		function createBackingFile(obj)
+			save(obj.BackingFile.fullPath,'obj','-v7.3');
+		end
+
+		function saveToBackingFile(obj)
+			save(obj.BackingFile.fullPath,'obj','-v7.3');
+		end
+		
+		function saveToUserSelectableCopyMat(obj)
+			% allows the user to select a file path to save a copy of the current annotation object out to a .mat file.
+			uisave({'obj'},['UAnnotations-CellROI-', '0', '.mat'])
+		end 
+        
+    end %% end backing file methods block
+    
+    methods (Static)
+      % Creates an explictly typed annotation from a regular one.
+      function obj = loadFromExistingBackingFile(backingFilePath)
+		if ~exist('backingFilePath','var')
+			backingFilePath = 'UserAnnotationsBackingFile.mat';
+		end  
+		 L = load(backingFilePath,'obj');
+		 obj = L.obj;
+	  end
+	
+	end % end methods static
+    
 end
 
