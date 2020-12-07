a0f702b39329126e83408062aae99998dd85dd08
CommanderPho <PhoHale@gmail.com>
2020-12-07 11:57:19 -0500

    Refactored interactive slider plotting

--- Post Suite2p Processing/Testing/ShowSliderControlledLinkedInteractiveFigures.m
+++ Post Suite2p Processing/Testing/ShowSliderControlledLinkedInteractiveFigures.m
@@ -8,9 +8,15 @@
 
 addpath(genpath('../../helpers'));
 
+final_data_explorer_obj = final_data_explorer_obj.computeCurveAnalysis();
+
+
 %% Options:
+should_show_2d_plot = true;
+should_show_3d_mesh_plot = false;
 should_show_masking_plot = false;
 should_show_stimulus_traces_plot = true;
+should_show_stimulus_traces_custom_data_plot = true;
 
 temp.cellRoiIndex = 1;
 
@@ -24,33 +30,56 @@ end
 iscInfo.curr_i = temp.cellRoiIndex;
 iscInfo.NumberOfSeries = length(final_data_explorer_obj.uniqueComps);
 
+linkedFigureHandles = [];
+plot_callbacks = {};
+
 % Build or get the figures that will be controlled by the slider.
-extantFigH_plot_2d = createFigureWithNameIfNeeded('Slider Controlled 2D Plot');
-extantFigH_plot_3d = createFigureWithNameIfNeeded('Slider Controlled 3D Mesh Plot');
+if should_show_2d_plot
+	extantFigH_plot_2d = createFigureWithTagIfNeeded('isc2DPlot');
+	linkedFigureHandles(end+1) = extantFigH_plot_2d;
+	plot_2d_callback = @(curr_i) (pho_plot_2d(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.finalOutPeaksGrid, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_2d, curr_i));
+	plot_callbacks{end+1} = plot_2d_callback;
+end
 
-linkedFigureHandles = [extantFigH_plot_2d extantFigH_plot_3d];
+if should_show_3d_mesh_plot
+	extantFigH_plot_3d = createFigureWithTagIfNeeded('isc3DMeshPlot');
+	linkedFigureHandles(end+1) = extantFigH_plot_3d;
+	mesh_3d_plot_callback = @(curr_i) (pho_plot_3d_mesh(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.finalOutPeaksGrid, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_3d, curr_i));
+	plot_callbacks{end+1} = mesh_3d_plot_callback;
+end
 
 if should_show_masking_plot
-    extantFigH_plot_masking = createFigureWithNameIfNeeded('Slider Controlled Masking Plot');
+    extantFigH_plot_masking = createFigureWithTagIfNeeded('iscMaskingPlot');
     linkedFigureHandles(end+1) = extantFigH_plot_masking;
-end
-if should_show_stimulus_traces_plot
-    extantFigH_plot_stimulus_traces = createFigureWithNameIfNeeded('Slider Controlled Stimulus Traces Plot');
-    linkedFigureHandles(end+1) = extantFigH_plot_stimulus_traces;
-end
-
-main_plot_callback = @(curr_i) (pho_plot_interactive_all(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.finalOutPeaksGrid, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_2d, extantFigH_plot_3d, curr_i));
-plot_callbacks = {main_plot_callback};
-
-if should_show_masking_plot
-    secondary_plot_callback = @(curr_i) (pho_plot_interactive_masking_all(final_data_explorer_obj.dateStrings, final_data_explorer_obj.compMasks, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_masking, curr_i));
+	secondary_plot_callback = @(curr_i) (pho_plot_interactive_masking_all(final_data_explorer_obj.dateStrings, final_data_explorer_obj.compMasks, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_masking, curr_i));
     plot_callbacks{end+1} = secondary_plot_callback;
 end
 
 if should_show_stimulus_traces_plot
-    stimulus_traces_plot_callback = @(curr_i) (pho_plot_stimulus_traces(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.uniqueStimuli, final_data_explorer_obj.traceTimebase_t, final_data_explorer_obj.active_DFF.TracesForAllStimuli, final_data_explorer_obj.redTraceLinesForAllStimuli, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_stimulus_traces, curr_i));
+    extantFigH_plot_stimulus_traces = createFigureWithTagIfNeeded('iscStimulusTracesPlot');
+    linkedFigureHandles(end+1) = extantFigH_plot_stimulus_traces;
+	stimulus_traces_plot_callback = @(curr_i) (pho_plot_stimulus_traces(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.uniqueStimuli, final_data_explorer_obj.traceTimebase_t, final_data_explorer_obj.active_DFF.TracesForAllStimuli, final_data_explorer_obj.redTraceLinesForAllStimuli, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_stimulus_traces, curr_i));
     plot_callbacks{end+1} = stimulus_traces_plot_callback;
 end
+
+if should_show_stimulus_traces_custom_data_plot
+    extantFigH_plot_stimulus_traces_extra = createFigureWithTagIfNeeded('iscStimulusTracesExtendedInfoPlot');
+    linkedFigureHandles(end+1) = extantFigH_plot_stimulus_traces_extra;
+	stimulus_traces_extras_plot_callback = @(curr_i) (pho_plot_stimulus_traces(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.uniqueStimuli, final_data_explorer_obj.traceTimebase_t, final_data_explorer_obj.active_DFF.TracesForAllStimuli, final_data_explorer_obj.computedRedTraceLinesAnalyses.second_derivative, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_stimulus_traces_extra, curr_i));
+    plot_callbacks{end+1} = stimulus_traces_extras_plot_callback;
+end
+
+
+% if should_show_stimulus_traces_plot
+%     stimulus_traces_plot_callback = @(curr_i) (pho_plot_stimulus_traces(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.uniqueStimuli, final_data_explorer_obj.traceTimebase_t, final_data_explorer_obj.active_DFF.TracesForAllStimuli, final_data_explorer_obj.redTraceLinesForAllStimuli, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_stimulus_traces, curr_i));
+%     plot_callbacks{end+1} = stimulus_traces_plot_callback;
+% end
+
+% if should_show_stimulus_traces_custom_data_plot
+%     stimulus_traces_extras_plot_callback = @(curr_i) (pho_plot_stimulus_traces(final_data_explorer_obj.dateStrings, final_data_explorer_obj.uniqueAmps, final_data_explorer_obj.uniqueFreqs, final_data_explorer_obj.uniqueStimuli, final_data_explorer_obj.traceTimebase_t, final_data_explorer_obj.active_DFF.TracesForAllStimuli, final_data_explorer_obj.computedRedTraceLinesAnalyses.second_derivative, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies, extantFigH_plot_stimulus_traces_extra, curr_i));
+%     plot_callbacks{end+1} = stimulus_traces_extras_plot_callback;
+% end
+
 
 slider_controller = fnBuildCallbackInteractiveSliderController(iscInfo, plot_callbacks);
 % PhoBuildSpatialTuning;
@@ -78,15 +107,15 @@ fig_layout_manager_obj.bindAlignedTopTargetEdgeToBottomReferenceEdge(relative_fi
 
 %% Plot Helper Functions:
 %% Plot function called as a callback on update
-function pho_plot_interactive_all(dateStrings, uniqueAmps, uniqueFreqs, finalOutPeaksGrid, multiSessionCellRoi_CompListIndicies, extantFigH_2d, extantFigH_3d, curr_cellRoiIndex)
-    if isvalid(extantFigH_2d)
-        pho_plot_2d(dateStrings, uniqueAmps, uniqueFreqs, finalOutPeaksGrid, multiSessionCellRoi_CompListIndicies, extantFigH_2d, curr_cellRoiIndex);
-    end
+% function pho_plot_interactive_all(dateStrings, uniqueAmps, uniqueFreqs, finalOutPeaksGrid, multiSessionCellRoi_CompListIndicies, extantFigH_2d, extantFigH_3d, curr_cellRoiIndex)
+%     if isvalid(extantFigH_2d)
+%         pho_plot_2d(dateStrings, uniqueAmps, uniqueFreqs, finalOutPeaksGrid, multiSessionCellRoi_CompListIndicies, extantFigH_2d, curr_cellRoiIndex);
+%     end
 
-    if isvalid(extantFigH_3d)
-        pho_plot_3d_mesh(dateStrings, uniqueAmps, uniqueFreqs, finalOutPeaksGrid, multiSessionCellRoi_CompListIndicies, extantFigH_3d, curr_cellRoiIndex);
-    end
-end
+%     if isvalid(extantFigH_3d)
+%         pho_plot_3d_mesh(dateStrings, uniqueAmps, uniqueFreqs, finalOutPeaksGrid, multiSessionCellRoi_CompListIndicies, extantFigH_3d, curr_cellRoiIndex);
+%     end
+% end
 
 
 function pho_plot_interactive_masking_all(dateStrings, compMasks, multiSessionCellRoi_CompListIndicies, extantFigH_masking, curr_cellRoiIndex)
