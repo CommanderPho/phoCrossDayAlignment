2caf2f066026b2bd02314445441b0078f9a7c15d
CommanderPho <PhoHale@gmail.com>
2020-12-15 02:24:03 -0500

    Awesome working selection

--- helpers/Plotting/Utilities/fnPlotHelper_SetupStimulusSubplot.m
+++ helpers/Plotting/Utilities/fnPlotHelper_SetupStimulusSubplot.m
@@ -11,8 +11,35 @@ function fnPlotHelper_SetupStimulusSubplot(final_data_explorer_obj, numRows, num
 	end
 
 	set(curr_subplot_axes, 'Tag', num2str(curr_linear_stimulus_index));
-	curr_subplot_axes.ButtonDownFcn = @(hSrc, event) disp(event.Source.Tag);
+% 	curr_subplot_axes.ButtonDownFcn = @(hSrc, event) disp(event.Source.Tag);
+    curr_subplot_axes.ButtonDownFcn = @(hSrc, event) fnPlotHelper_ToggleSubplotSelection(event.Source);
 
+
+    function [updatedSelectionStatus] = fnPlotHelper_ToggleSubplotSelection(curr_subplot_axes)
+        stimulusTagNum = str2num(curr_subplot_axes.Tag); 
+        fprintf('fnPlotHelper_ToggleSubplotSelection(...): subplot corresponding to stimulus %d\n', stimulusTagNum);
+        prev_was_selected = curr_subplot_axes.Selected;
+        updatedSelectionStatus = ~prev_was_selected;
+        fnPlotHelper_UpdateSelectedSubplot(curr_subplot_axes, updatedSelectionStatus);
+    end
+
+    function fnPlotHelper_UpdateSelectedSubplot(curr_subplot_axes, is_selected)
+        curr_subplot_axes.Selected = is_selected;
+        if curr_subplot_axes.Selected
+            box(curr_subplot_axes,'on');
+            % Set the remaining axes properties
+            set(curr_subplot_axes,'BoxStyle','full','LineWidth',2,'Color',[0.941176470588235 0.941176470588235 0.941176470588235],'XColor',...
+                [0.301960784313725 0.745098039215686 0.933333333333333],'YColor',...
+                [0.301960784313725 0.749019607843137 0.929411764705882]);
+        else
+            box(curr_subplot_axes,'off');
+            % Set the remaining axes properties
+            set(curr_subplot_axes,'BoxStyle','full','LineWidth',0.5,'Color',[1 1 1],'XColor',...
+                [0.15,0.15,0.15],'YColor',...
+                [0.15,0.15,0.15]);
+        end
+        
+    end
     
 end % end fnPlotHelper_SetupStimulusSubplot
 
