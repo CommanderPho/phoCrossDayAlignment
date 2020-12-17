4df794a2413cb295e135aa272970e625ab20e04a
CommanderPho <PhoHale@gmail.com>
2020-12-17 17:54:40 -0500

    working

--- Post Suite2p Processing/FISSA_Result_Loading.m
+++ Post Suite2p Processing/FISSA_Result_Loading.m
@@ -14,7 +14,7 @@ if ~exist('loaded_fissa_data','var')
     loaded_fissa_data = load(phoPipelineOptions.default_fissa_file_path);
     disp('done.')
 end
-[outputs] = process_loaded_FISSA_result(loaded_fissa_data);
+[outputs] = process_loaded_FISSA_result(loaded_fissa_data, phoPipelineOptions);
 
 
 function [outputs] = process_loaded_FISSA_result(fissa_data, phoPipelineOptions)
@@ -49,27 +49,48 @@ function [outputs] = process_loaded_FISSA_result(fissa_data, phoPipelineOptions)
         fprintf('WARNING: the loaded fissa data is missing the computed dF/F data. Did you run the full python script?');
     end
     
-    
     includedCompMask = ~phoPipelineOptions.loadedFilteringData.manualRoiFilteringResults.final_is_Excluded;
     filteredCellFields = outputs.cellFields(includedCompMask);
     % filteredCellFieldOriginalIndex: the index that maps to the original [1, 82] range.
-    filteredCellFieldOriginalIndex = 1:outputs.numCells;
-    filteredCellFieldOriginalIndex = filteredCellFieldOriginalIndex(includedCompMask);
+    filteredCellFieldOriginalIndicies = 1:outputs.numCells;
+    filteredCellFieldOriginalIndicies = filteredCellFieldOriginalIndicies(includedCompMask)';
     filteredNumCells = length(filteredCellFields);
     
     % Loop through the filtered number of cells
     for i = 1:filteredNumCells
         curr_cell_id_name = filteredCellFields{i};
-        curr_cell_id_original_index = filteredCellFieldOriginalIndex(i);
-%         curr_cell.df_raw = 
+        curr_cell_id_original_index = filteredCellFieldOriginalIndicies(i);
+
+        % - `result.cell0.trial0(1,:)` final extracted cell signal
+        % - `result.cell0.trial0(2,:)` contaminating signal
+        % - `raw.cell0.trial0(1,:)` raw measured cell signal
+        % - `raw.cell0.trial0(2,:)` raw signal from first neuropil region
+
+        [~, ~, curr_cell.df_raw] = flattenAllFields(fissa_data.df_raw.(curr_cell_id_name), 2);
+        numCombinedDatapoints = length(curr_cell.df_raw);
+        [~, ~, curr_cell.df_result] = flattenAllFields(fissa_data.df_result.(curr_cell_id_name), 2);        
+        [~, ~, curr_cell.raw] = flattenAllFields(fissa_data.raw.(curr_cell_id_name), 2);
+        [~, ~, curr_cell.result] = flattenAllFields(fissa_data.result.(curr_cell_id_name), 2);
         
-        [~, ~, curr_cell.df_result] = flattenAllFields(df_result.(curr_cell_id_name), 2);
-        [~, ~, curr_cell.df_raw] = flattenAllFields(df_raw.(curr_cell_id_name), 2);
+        is_first_cell = (i == 1);
         
-        [~, ~, outputValues] = flattenAllFields(loaded_fissa_data.df_raw.cell0, 2);
+        if is_first_cell
+            % Pre-allocate the output structures now that we know how big they are:
+            outputs.results.df_raw = zeros([filteredNumCells numCombinedDatapoints]); 
+            outputs.results.df_result = zeros([filteredNumCells size(curr_cell.df_result,1) size(curr_cell.df_result,2)]);
+            outputs.results.raw = zeros([filteredNumCells size(curr_cell.raw,1) size(curr_cell.raw,2)]); 
+            outputs.results.result = zeros([filteredNumCells size(curr_cell.result,1) size(curr_cell.result,2)]);
+        end
+        
+        outputs.results.df_raw(i,:) = curr_cell.df_raw;
+        outputs.results.df_result(i,:,:) = curr_cell.df_result;
+        outputs.results.raw(i,:,:) = curr_cell.raw;
+        outputs.results.result(i,:,:) = curr_cell.result;
     end
 
-    
+    outputs.includedCompMask = includedCompMask;
+    outputs.filteredCellFields = filteredCellFields;
+    outputs.filteredCellFieldOriginalIndicies = filteredCellFieldOriginalIndicies;
 %     length(fieldnames(finalDataStruct.anm265.session_20200117.imgData)) - 82
 
 
