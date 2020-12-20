2e3c8e6917bcb026acb5d12bc09670045f7f9221
CommanderPho <PhoHale@gmail.com>
2020-12-20 13:02:08 -0500

    Pre-commit

--- helpers/Classes/Plotting/CustomCharts/PolygonRoiChart.m
+++ helpers/Classes/Plotting/CustomCharts/PolygonRoiChart.m
@@ -7,8 +7,8 @@ classdef PolygonRoiChart < matlab.graphics.chartcontainer.ChartContainer
         Color = [1 0 0]
     end
     properties(Access = private,Transient,NonCopyable)
-        LineObject
-        PolygonObjects
+        OutlineBordersLineArray (:,1) matlab.graphics.chart.primitive.Line
+        PolygonObjects (:,1) matlab.graphics.chart.primitive.Patch
     end
     methods(Access = protected)
         function setup(obj)
@@ -18,31 +18,65 @@ classdef PolygonRoiChart < matlab.graphics.chartcontainer.ChartContainer
             % Create Patch and Line objects
             obj.PatchObject = patch(ax,NaN,NaN,'r','FaceAlpha',0.2,...
                 'EdgeColor','none');
+
+			obj.PolygonObjects = patch(ax,NaN,NaN,'r','FaceAlpha',0.2,...
+                'EdgeColor','none');
+
             hold(ax,'on')
-            obj.LineObject = plot(ax,NaN,NaN);
+            obj.OutlineBordersLineArray = plot(ax,NaN,NaN);
             
             % Turn hold state off
             hold(ax,'off')
         end
         function update(obj)
             % Update XData and YData of Line
-            obj.LineObject.XData = obj.XData;
-            obj.LineObject.YData = obj.YData;
+			
+			% obj.OutlineBordersLineArray = plot(ax,obj.XData,obj.YData);
+            % hold(ax,'on')
+
+            obj.OutlineBordersLineArray.XData = obj.XData;
+            obj.OutlineBordersLineArray.YData = obj.YData;
             
             % Update patch XData and YData
             x = obj.XData;
-            obj.PatchObject.XData = [x x(end:-1:1)];
+            obj.PolygonObjects.XData = [x x(end:-1:1)];
             y = obj.YData;
             c = obj.ConfidenceMargin;
-            obj.PatchObject.YData = [y+c y(end:-1:1)-c];
+            obj.PolygonObjects.YData = [y+c y(end:-1:1)-c];
             
             % Update colors
-            obj.LineObject.Color = obj.Color;
-            obj.PatchObject.FaceColor = obj.Color;
+            obj.OutlineBordersLineArray.Color = obj.Color;
+            obj.PolygonObjects.FaceColor = obj.Color;
             
             % Update markers
-            obj.LineObject.Marker = obj.MarkerSymbol;
+            obj.OutlineBordersLineArray.Marker = obj.MarkerSymbol;
         end
     end
-end
+
+	methods(Access = public)
+
+		function update_comp_polys(activeRoiCells)
+			%update_comp_polys:
+			for i = 1:length(activeRoiCells)
+				currCellPolys = activeRoiCells{i};
+				for j = 1:length(currCellPolys)
+					curr_poly = currCellPolys{j};
+					x = curr_poly(:, 2);
+					y = curr_poly(:, 1);
+					plot(x,y)
+					fill(x,y,'r')
+			%         alpha(transparency);
+					xlim([1 512]);
+					ylim([1 512]);
+					
+				end % end for j
+			end % end for i
+
+		end % end function update_comp_polys
+
+
+	end % end public methods block
+
+
+end % end classdef
 
